17eedc420e7fbae260846dbf21eb2062a47e7907
Eldoza1 <chris.mendoza@drivindealer.com>
2019-02-11 10:32:11 -0600

    Added twosum answer

--- Javascript_Problems/twosum.js
+++ Javascript_Problems/twosum.js
@@ -8,3 +8,39 @@
 //
 // Because nums[0] + nums[1] = 2 + 7 = 9,
 // return [0, 1]
+
+// Test cases:
+// Input: 
+// sampleSet = [3, 3]
+// Target = 6
+// Expected: [0, 1]
+
+const twoSum = function(nums, target) {
+  const comp = {};
+  for (let i = 0; i < nums.length; i++){
+      if (comp[nums[i]] >= 0){
+          console.log('comp dyamic key ===>', [comp[nums[i]], i])
+          return [comp[nums[i]],i]
+      }
+      comp[target - nums[i]] = i
+  }
+};
+
+var sampleSet = [2,7,11,15]
+var sampleTarget = 9
+
+// var twoSum  = function(numsArr, target) {
+//   const indicesToReturn = []
+//   for (let i = 0; i < numsArr.length; i++) {
+//     for (let j = i + 1; j < numsArr.length; j++){
+//       if ((numsArr[i] + numsArr[j]) === target) {
+//         indicesToReturn.push(numsArr.indexOf(numsArr[i]))
+//         indicesToReturn.push(numsArr.indexOf(numsArr[j]))
+//       }
+//     }
+//   }
+
+//   return indicesToReturn
+// }
+
+console.log(twoSum(sampleSet, sampleTarget))
